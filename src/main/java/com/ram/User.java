81fed0139e37816f358fda4d8163bc064b368aa8
Ramsinghh <yadavramsingh329@gmail.com>
2022-07-27 18:06:10 +0530

    Users controller Added

---
+++ src/main/java/com/ram/User.java
@@ -0,0 +1,27 @@
+package com.ram;
+
+public class User {
+    private String name;
+    public Integer rollNo;
+
+    public User(String name, Integer rollNo) {
+        this.name = name;
+        this.rollNo = rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(Integer rollNo) {
+        this.rollNo = rollNo;
+    }
+}
--- src/main/java/com/ram/UserController.java
+++ src/main/java/com/ram/UserController.java
@@ -1,56 +1,72 @@
 package com.ram;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
 public class UserController {
 
-    List<User> list=new ArrayList<User>(){
+    List<User> list = new ArrayList<User>() {
         {
-            add(new User("ramu",1));
-            add(new User("shyam",2));
-            add(new User("raj",3));
-            add(new User("vikash",4));
-        }};
+            add(new User("ramu", 1));
+            add(new User("shyam", 2));
+            add(new User("raj", 3));
+            add(new User("vikash", 4));
+        }
+    };
 
     @GetMapping("/users")
-    public List<User> getAllUser(@RequestParam(defaultValue = "0") String name){
+    public List<User> getAllUser(@RequestParam(defaultValue = "") String name) {
         return list.stream()
-                .filter(x->x.getName().contains(name))
+                .filter(x -> x.getName().contains(name))
                 .collect(Collectors.toList());
     }
+    @GetMapping("/users/{rollNo}")
+    public User getUser(@PathVariable Integer rollNo) {
+        Optional<User> optionalUser = list.stream()
+                .filter(x -> x.rollNo == rollNo)
+                .findAny();
 
-    //json - javascript object notation
-}
-
-class User{
-    private String name;
-    public Integer rollNo;
-
-    public User(String name, Integer rollNo) {
-        this.name = name;
-        this.rollNo = rollNo;
+        if (optionalUser.isPresent()) {
+            return optionalUser.get();
+            }
+        else {
+            throw new RuntimeException("User not found");
+        }
     }
 
-    public String getName() {
-        return name;
+    @PostMapping("/users")
+    public String save(@RequestBody User user) {
+        list.add(user);
+        return "user saved successfully";
     }
 
-    public void setName(String name) {
-        this.name = name;
+    @PutMapping("/users")
+    public String update(@RequestBody User user) {
+        Optional<User> optionalUser = list.stream()
+                .filter(x -> x.rollNo == user.rollNo)
+                .findAny();
+
+        if (optionalUser.isPresent()) {
+            optionalUser.get().setName(user.getName());
+            return "Updated successfully";
+        }
+        return "User not found";
     }
 
-    public Integer getRollNo() {
-        return rollNo;
-    }
-
-    public void setRollNo(Integer rollNo) {
-        this.rollNo = rollNo;
+    @DeleteMapping("/users/{roll}")
+    public String delete(@PathVariable Integer roll) {
+        Optional<User> optionalUser = list.stream()
+                .filter(x -> x.rollNo == roll)
+                .findAny();
+        if (optionalUser.isPresent()) {
+            list.remove(optionalUser.get());
+            return "Deleted successfully";
+        }
+        return "User not found";
     }
 }
